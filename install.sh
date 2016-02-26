aa4b833c4ea90c36fb7d1e37a42f8e8b1d4a5fb7
Arthur Schneider <dev.schneiart@gmail.com>
2016-02-26 15:02:35 +0100

    Refactor install.sh file, use methods, and $USERNAME variable

--- install.sh
+++ install.sh
@@ -1,47 +1,55 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles
-# in ~/dotfiles
-############################
 
-########## Variables
+########## Variables ###########
 
 dir=~/.dotfiles
 olddir=~/.dotfiles_old
 files="vimrc zshrc"
-user="w1nd"
+user=$USERNAME
 
-##########
+################################
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+########## Functions ###########
 
-# move any existing dotfiles in homedir to dotfiles_old directory,
-# then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/.dotfiles_old/
+backup_oldfiles(){
+ mkdir -p $olddir
+ cd $dir
+ 
+ for file in $files; do
+     echo "Moving any existing dotfiles from ~ to $olddir"
+     mv ~/.$file ~/.dotfiles_old/
+ done
+}
+
+
+link_the_files(){
+  for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file/.$file ~/.$file
-done
+  done
+ 
+  echo "Creating symlink for .gitconfig in home directory."
+  cd ~
+  ln -s $dir/git/.gitconfig ~/.gitconfig
+ 
+  echo "Creating symlink for custom theme in .oh-my-zsh."
+  cd $HOME/.oh-my-zsh/custom/themes
+  ln -s $dir/zshrc/ruhtra.zshrc-theme ruhtra.zshrc-theme
 
-# add link fot gitconfig
-cd ~
-ln -s $dir/git/.gitconfig ~/.gitconfig
+}
 
-# make link for custom zsh theme
-cd $HOME/.oh-my-zsh/custom/themes
-ln -s $dir/zshrc/ruhtra.zshrc-theme ruhtra.zshrc-theme
+################################
+
+
+############## Main ############
+
+backup_oldfiles
+link_the_files
 
 #enable all the .dotfiles
 source ~/.zshrc
 
 echo "Everything done try it out!"
+################################
+
